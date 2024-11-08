8a0ae408d2fee0cb8cd13d3627bc5b6f4d1ae0bf
max-lt <maxime.tissier@gmail.com>
2024-11-08 20:05:24 +0100

    Change time str fn scope

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,6 @@ pub use block::BlockReaderOptions;
 // Re-export chrono types
 pub type DateTime = chrono::DateTime<chrono::Utc>;
 
-pub (crate) fn time_str(time: DateTime) -> String {
+pub fn time_str(time: DateTime) -> String {
     time.to_string().replace(" UTC", "")
 }
